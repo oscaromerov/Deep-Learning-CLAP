b04a4e1fe1a787f9c5a4b7ee7d950a4711945dd3
RicFerGas <ricardo.fdz.gasca@gmail.com>
2023-12-14 00:28:41 +0100

    Individual prediction functions added for testing models

--- dataset_creator/generes_dataset_creator.py
+++ dataset_creator/generes_dataset_creator.py
@@ -16,20 +16,20 @@ input_audios = {
     'Rock': "/Users/fidenciofernandez/OneDrive/IMT/M1S2/DeepLearning/Proj/Rock.mp3"
 }
 
-output_folder = "/Users/fidenciofernandez/OneDrive/IMT/M1S2/DeepLearning/Proj/GenresDataSet"
+output_folder = "/Users/fidenciofernandez/OneDrive/IMT/M1S2/DeepLearning/Proj/GenresDataSet200Samp"
 
 # Step 2: Create Output Folder if Not Exists
 os.makedirs(output_folder, exist_ok=True)
 
 # Step 3: Extract Audio Segments and Create CSV
-def extract_audio_segments(audio_path, sentiment, output_folder, segment_duration=10, starting_number=1):
+def extract_audio_segments(audio_path, sentiment, output_folder, segment_duration=10, starting_number=1, max_segments=250):
     audio = AudioSegment.from_mp3(audio_path)
     total_duration = len(audio)
     segment_start = 0
     segment_number = starting_number
     csv_data = []
 
-    while segment_start < total_duration:
+    while segment_start < total_duration and segment_number - starting_number < max_segments:
         segment_end = min(segment_start + segment_duration * 1000, total_duration)
         audio_segment = audio[segment_start:segment_end]
         segment_filename = f"audio_segment_{segment_number}.wav"
@@ -54,11 +54,11 @@ all_csv_data = []
 overall_segment_count = 1
 
 for sentiment, audio_path in input_audios.items():
-    csv_data, overall_segment_count = extract_audio_segments(audio_path, sentiment, output_folder, starting_number=overall_segment_count)
+    csv_data, overall_segment_count = extract_audio_segments(audio_path, sentiment, output_folder, starting_number=overall_segment_count, max_segments=250)
     all_csv_data.extend(csv_data)
 
 # Create CSV File
-csv_file_path = os.path.join(output_folder, "genres_music_labels.csv")
+csv_file_path = os.path.join(output_folder, "all_music_labels.csv")
 df = pd.DataFrame(all_csv_data)
 df.to_csv(csv_file_path, index=False)
 
